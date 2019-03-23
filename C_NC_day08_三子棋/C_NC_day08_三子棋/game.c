3c9b51fbccb3295a92411791743f5ffaa1fa3e53
Kiids <1980774293@qq.com>
2019-03-23 23:30:47 +0800

    三子棋

--- C_NC_day08_三子棋/C_NC_day08_三子棋/game.c
+++ C_NC_day08_三子棋/C_NC_day08_三子棋/game.c
@@ -121,22 +121,164 @@ static int IsFull(char board[ROW][COL], int row, int col)
 
 char IsWin(char board[ROW][COL], int row, int col)
 {
-	int i;
+	//int i;
+	//for (i = 0; i < row; i++)
+	//{
+	//	if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
+	//		return board[i][1];
+	//}
+	//for (i = 0; i < col; i++)
+	//{
+	//	if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != ' ')
+	//		return board[1][i];
+	//}
+	//if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	//	return board[1][1];
+	//else if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] != ' ')
+	//	return board[1][1];
+	//else if (IsFull(board, row, col))
+	//	return 'Q';
+	//return 'C';
+	int i = 0, j = 0;
+	int flag = 0;
+	//一行相等
 	for (i = 0; i < row; i++)
 	{
-		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
-			return board[i][1];
+		flag = 0;
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == '*')
+				;
+			else
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag != 1)
+			return '*';
 	}
-	for (i = 0; i < col; i++)
+
+	for (i = 0; i < row; i++)
 	{
-		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != ' ')
-			return board[1][i];
+		flag = 0;
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == '#')
+				;
+			else
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag != 1)
+			return '#';
 	}
-	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
-		return board[1][1];
-	else if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] != ' ')
-		return board[1][1];
-	else if (IsFull(board, row, col))
-		return 'Q';
-	return 'C';
+	
+	//一列相等
+	for (i = 0; i < row; i++)
+	{
+		flag = 0;
+		for (j = 0; j < col; j++)
+		{
+			if (board[j][i] == '*')
+				;
+			else
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag != 1)
+			return '*';
+	}
+
+	for (i = 0; i < row; i++)
+	{
+		flag = 0;
+		for (j = 0; j < col; j++)
+		{
+			if (board[j][i] == '#')
+				;
+			else
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag != 1)
+			return '#';
+	}
+	
+	//主对角线相等
+	flag = 0;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][i] == '*')
+			;
+		else
+		{
+			flag = 1;
+			break;
+		}
+		
+	}
+	if (flag != 1)
+		return '*';
+
+	flag = 0;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][i] == '#')
+			;
+		else
+		{
+			flag = 1;
+			break;
+		}
+
+	}
+	if (flag != 1)
+		return '#';
+
+	//副对角线相等
+	flag = 0;
+	for (i = 0, j = col - 1; i < row, j >= 0; i++, j--)
+	{
+		if (board[i][j] == '*')
+			;
+		else
+		{
+			flag = 1;
+			break;
+		}
+
+	}
+	if (flag != 1)
+		return '*';
+
+	flag = 0;
+	for (i = 0, j = col - 1; i < row, j >= 0; i++, j--)
+	{
+		if (board[i][j] == '#')
+			;
+		else
+		{
+			flag = 1;
+			break;
+		}
+
+	}
+	if (flag != 1)
+		return '#';
+
+	//不相等
+	if (flag)
+	{
+		if (IsFull(board, row, col))
+			return 'Q';
+		else
+			return 'C';
+	}
 }
