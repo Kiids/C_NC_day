4f049a7d42834267a8cd81a0128491978f8d5164
Kiids <1980774293@qq.com>
2019-04-09 23:57:32 +0800

    逻辑运算

--- C_NC_day11/C_NC_day11/C_NC_day11_2.c
+++ C_NC_day11/C_NC_day11/C_NC_day11_2.c
@@ -12,6 +12,31 @@
 //D说：C在胡说 
 //已知3个人说了真话，1个人说的是假话。 
 //现在请根据这些信息，写一个程序来确定到底谁是凶手。 
+
+void predict()
+{
+	int killer = 0;
+	// a, b, c, d --> 1, 2, 3, 4
+	for (killer = 1; killer <= 4; killer++)
+	{
+		if ((killer != 1) + (killer == 3) + (killer == 4) + (killer != 4) == 3)
+		{
+			printf("凶手是：%c\n", killer + 'A' - 1);
+		}
+	}
+}
+void predict2()
+{
+	char killer = 0;
+	for (killer = 'A'; killer <= 'D'; killer++)
+	{
+		if ((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D') == 3)
+		{
+			printf("凶手是：%c\n", killer);
+		}
+	}
+}
+
 int main()
 {
 	int murder = 0;
